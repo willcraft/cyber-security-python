13e9e5954d074f2d61259c930fa1939944b42df7
Yoshitaka Sasaki <y_sasaki@willcraft.jp>
2018-02-03 15:31:04 +0900

    2章 TCPサーバ

--- chapter/2/2-tcpclient.py
+++ chapter/2/2-tcpclient.py
@@ -1,11 +1,11 @@
 import socket
 
-target_host = "www.google.co.jp"
-target_port = 80
+target_host = "127.0.0.1"
+target_port = 9999
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect((target_host, target_port))
-client.send("GET / HTTP/1.1\r\nHost: google.com\r\n\r\n")
+client.send("TEST")
 
 response = client.recv(4096)
 
